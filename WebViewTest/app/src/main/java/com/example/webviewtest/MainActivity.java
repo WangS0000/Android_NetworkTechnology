6ec6cdbb541fd3615448a6fcb8de1eaf8bedaf24
WangS0000 <374817364@qq.com>
2019-10-08 20:33:25 +0800

    百度界面

--- WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
+++ WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -13,13 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.web_view);
-        //获取webView实例
         webView.getSettings().setJavaScriptEnabled(true);
-        //让WebView支持JavaScript脚本
         webView.setWebViewClient(new WebViewClient());
-        //将从网络传入的页面，仍然放在WebView中显示
-        webView.loadUrl("https://www.2345.com");
-        //传入网络地址
-        //切记:加入网络权限
+        webView.loadUrl("https://www.baidu.com");
     }
 }
